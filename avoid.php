b64b923e8f9be777228d60ed08f7a11912a7c50e
bgcolor <493046077@qq.com>
2015-08-18 11:38:25 +0800

    change to Chinese add outer link and banned link

---
+++ avoid.php
@@ -0,0 +1,3 @@
+<?php 
+header("Content-Type: text/html; charset=utf-8");
+echo '此内容不会出现不会作为pjax更新';
--- server.php
+++ server.php
@@ -3,12 +3,14 @@ function is_pjax(){
     return array_key_exists('HTTP_X_PJAX', $_SERVER) && $_SERVER['HTTP_X_PJAX'];
 }
 
+header("Content-Type: text/html; charset=utf-8");
+
 // 若为pjax只输出更新的内容，若为直接请求，输出整个页面
 if (is_pjax()) {
-    echo 'pjax content from server.php uuid ',uniqid();
+    echo '来自server.php的pjax返回 uuid: ',uniqid();
     
     if (!empty($_GET['a']) && !empty($_GET['b'])) {
-        echo '<br>params: ';
+        echo '<br>参数: ';
         echo 'a=',$_GET['a'],'b=',$_GET['b'];
     }
     
@@ -22,10 +24,12 @@ if (is_pjax()) {
     <title>Pjax Test</title>
 </head>
 <body>
-    <a href="server.php">normal link</a>
-    <a href="server.php?a=1&b=2">link with params(a=1&b=2)</a>
+    <a href="server.php">普通链接</a>
+    <a href="server.php?a=1&b=2">参数链接(a=1&b=2)</a>
+    <a href="http://www.baidu.com">外链</a>
+    <a href="avoid.php">阻止内部链接</a>
     <div id="container">
-        direct from server.php
+        直接请求server.php
     </div>
     <script src="jquery.js" type="text/javascript"></script>
     <script src="jquery.pjax.js" type="text/javascript"></script>
@@ -37,8 +41,12 @@ if (is_pjax()) {
             show: 'fade',  //展现的动画，支持默认和fade, 可以自定义动画方式，这里为自定义的function即可。
             cache: false,  //是否使用缓存
             storage: true,  //是否使用本地存储
-            titleSuffix: '', //标题后缀
-            filter: function(){},
+            titleSuffix: '来自pjax', //标题后缀
+            filter: function(href){
+                if(href.indexOf('avoid') != -1){
+                    return true;
+                }
+            },
             callback: function(){}
         });
     });
